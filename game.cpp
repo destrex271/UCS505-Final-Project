dd834adc975e2ace4f991030464d3a0d220f85dc
Akshat Jaimini <destrex271@gmail.com>
2024-04-11 13:51:10 +0530

    Added grid scaling functions

--- game.cpp
+++ game.cpp
@@ -7,6 +7,17 @@ namespace Game{
         this->grid_height = grid_height;
         pacmanObj = new Pacman(grid_height, grid_width);
         gameOver = false;
+
+        // Initialize game grid
+        for(int i = 0; i < this->grid_height; i++){
+            std::cout << i << std::endl;
+            std::vector<int> k;
+            for(int j = 0; j < this->grid_width; j++){
+                std::cout << j << std:: endl;
+                k.push_back(0);
+            }
+            this->grid.push_back(k);
+        }
     }
 
     void GameObject::displayData(){
@@ -17,4 +28,10 @@ namespace Game{
         std::cout << "\tGame Over: " << gameOver << std::endl;
         std::cout << "===============================" << std::endl << std::endl;
     }
+
+    std::pair<int, int> GameObject::scaleToWindowDim(std::pair<int, int> screen_dim){
+        return {screen_dim.first / (1.0 * this->grid_width), screen_dim.second / (1.0 * this->grid_height)};
+    }
+
+    void drawGameGrid(){}
 }
--- include/game.hpp
+++ include/game.hpp
@@ -1,12 +1,17 @@
 #include "pacman.hpp"
+#include <vector>
 
 namespace Game{
     class GameObject{
         public:
             Pacman* pacmanObj;
             int grid_height, grid_width;
+            std::vector<std::vector<int>> grid;
             bool gameOver; // If true terminate everything
             GameObject(int grid_height, int grid_width);
             void displayData();
+            std::pair<int, int> scaleToWindowDim(std::pair<int, int> screen_dim);
     };
+
+    void drawGameGrid();
 }
